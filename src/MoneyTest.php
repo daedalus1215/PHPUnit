bfc1615c27486abf8c752e85e5b52261586b7d02
LA <ladams1776@yahoo.com>
2016-08-27 11:59:28 -0400

    getting error about Argument #3

---
+++ src/MoneyTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace PHPFun;
+use PHPUnit\Framework\TestCase;
+include(__DIR__.'\..\vendor\autoload.php');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MoneyTest
+ *
+ * @author the_admin
+ */
+class MoneyTest extends TestCase
+{
+    public function testCanBeNegated()
+    {
+        // Arrange
+        $a = new Money(1);
+
+        // Act
+        $b = $a->negate();
+
+        // Assert
+        $this->assertEquals(-1, $b->getAmount());
+    }
+}
