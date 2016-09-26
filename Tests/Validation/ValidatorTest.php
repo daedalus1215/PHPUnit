391bc74cd88b15523a3539eeebabab9cd13e18a8
larry <ladams1776@yahoo.com>
2016-09-25 20:30:40 -0400

    Created first test

--- Tests/Validation/ValidatorTest.php
+++ Tests/Validation/ValidatorTest.php
@@ -1,9 +1,22 @@
 <?php
 namespace Acme\Tests;
 
+use Acme\Http\Response;
+use Acme\Http\Request;
+use Acme\Validation\Validator;
 
 
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
-    
+    public function testGetIsValidReturnsTrue()
+    {
+        $request = new Request([]);
+        $response = new Response($request);
+        
+        $validator = new Validator($request, $response);
+        
+        $validator->setIsValid(true);
+        
+        $this->assertTrue($validator->getIsValid());        
+    }
 }
