56be5d6e733323cb8080b7bde883d96e3710a381
larry <ladams1776@yahoo.com>
2016-09-25 22:51:37 -0400

    check for invalid data for email

--- Tests/Validation/ValidatorTest.php
+++ Tests/Validation/ValidatorTest.php
@@ -62,8 +62,17 @@ class ValidatorTest extends \PHPUnit_Framework_TestCase
         
         $errors = $validator->check(['mintype' => 'email']);
         
-        $this->assertCount(0, $errors);
-        
+        $this->assertCount(0, $errors);            
+    }
     
+    public function testCheckForEmailWithInvalidData()
+    {
+        $request = new Request(['mintype' => 'whatever']); //simulate a field with the name of mintype as a get variable, with the value yellow.
+        $response = new Response($request);        
+        $validator = new Validator($request, $response);
+        
+        $errors = $validator->check(['mintype' => 'email']);
+        
+        $this->assertCount(1, $errors);
     }
 }
